87541c2c3a70b1703cab3a3e0f8376a44a9f6d19
Json Bourne <jsonbourneagain@outlook.com>
2020-05-03 01:57:21 +0530

    Wrote in-place algo to reverse an array

---
+++ DSA/InPlaceReversingOfArray/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace InPlaceReversingOfArray
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+
+        public static void Reverse(char[] arrayOfChars)
+        {
+            var length = arrayOfChars.Length;
+
+            // Reverse input array of characters in place
+            if (arrayOfChars.Length > 0)
+            {
+                for (int i = 0; i < length / 2; i++)
+                {
+                    var temp = arrayOfChars[i];
+                    arrayOfChars[i] = arrayOfChars[length - 1 - i];
+                    arrayOfChars[length - 1 - i] = temp;
+                }
+            }
+        }
+    }
+}
