82c7a028e53ad71a3517e39f713ad6f4a636f208
Json Bourne <jsonbourneagain@outlook.com>
2020-05-04 02:02:30 +0530

    Today's practice

---
+++ DSA/InPlaceReversingOfWords/Program.cs
@@ -0,0 +1,77 @@
+﻿using System;
+
+namespace InPlaceReversingOfWords
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            char[] message = { 'c', 'a', 'k', 'e', ' ',
+                       'p', 'o', 'u', 'n', 'd', ' ',
+                       's', 't', 'e', 'a', 'l'};
+
+            ReverseWords(message);
+            Console.WriteLine(message);
+        }
+
+        // Question
+        // Your team is scrambling to decipher a recent message, worried it's a plot to break into a major European National Cake Vault.
+        // The message has been mostly deciphered, but all the words are backward! Your colleagues have handed off the last step to you.
+        // Write a method ReverseWords() that takes a message as an array of characters and reverses the order of the words in place.
+
+        //char[] message = { 'c', 'a', 'k', 'e', ' ',
+        //           'p', 'o', 'u', 'n', 'd', ' ',
+        //           's', 't', 'e', 'a', 'l'};
+
+        //ReverseWords(message);
+
+        //Console.WriteLine("{0}", new string (message));
+        // prints: "steal pound cake"
+
+        public static void ReverseWords(char[] message)
+        {
+            // Decode the message by reversing the words
+
+            int leftPointer = 0;
+            int rightPointer = message.Length - 1;
+            Swap(message, ref leftPointer, ref rightPointer);
+
+            // reset the left and right pointers
+            leftPointer = 0;
+            rightPointer = 0;
+
+            for (int i = 0; i <= message.Length; i++)
+            {
+                if (i == message.Length || Char.IsWhiteSpace(message[i])) // better than reversing the last word separately.. line 57
+                {
+                    rightPointer = i - 1;
+
+                    Swap(message, ref leftPointer, ref rightPointer);
+
+                    // move left and right pointers forward to the next word
+                    leftPointer = i + 1;
+                    rightPointer = i;
+                }
+                rightPointer++;
+            }
+
+            //swap the last word left
+            //rightPointer = message.Length - 1;
+            //Swap(message, ref leftPointer, ref rightPointer);
+
+        }
+
+        private static void Swap(char[] message, ref int leftPointer, ref int rightPointer)
+        {
+            while (leftPointer < rightPointer)
+            {
+                var temp = message[leftPointer];
+                message[leftPointer] = message[rightPointer];
+                message[rightPointer] = temp;
+
+                leftPointer++;
+                rightPointer--;
+            }
+        }
+    }
+}
---
+++ DSA/MergeTwoSortedArraysIntoOneArray/Program.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Linq;
+
+namespace MergeTwoSortedArraysIntoOneArray
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //var myArray = new int[] { 2, 4, 6, 8 };
+            //var alicesArray = new int[] { 1, 7 };
+            //var myArray = new int[] { };
+            //var alicesArray = new int[] { };
+            var myArray = new int[] { };
+            var alicesArray = new int[] { 1, 2, 3 };
+            //var myArray = new int[] { 5, 6, 7 };
+            //var alicesArray = new int[] { };
+            //var myArray = new int[] { 2, 4, 6 };
+            //var alicesArray = new int[] { 1, 3, 7 };
+            //var myArray = new int[] { 2, 4, 6, 8, 9,10,11 };
+            //var alicesArray = new int[] { 1, 7,20 };
+
+            var a = MergeArrays(myArray, alicesArray);
+
+            Console.WriteLine("Hello World!");
+        }
+
+        public static int[] MergeArrays(int[] myArray, int[] alicesArray)
+        {
+            // Combine the sorted arrays into one large sorted array
+            var mergedArray = new int[myArray.Length + alicesArray.Length];
+            int myPointer = default;
+            int alicesPointer = default;
+
+            for (int i = 0; i < myArray.Length + alicesArray.Length; i++)
+            {
+                int headOfMyArray = default;
+                int headOfAlicesArray = default;
+                bool aliceHasGotNoElements = false;
+                bool iHaveGotNoElements = false;
+
+                if (myPointer < myArray.Length)
+                    headOfMyArray = myArray[myPointer];
+                else
+                    iHaveGotNoElements = true;
+
+                if (alicesPointer < alicesArray.Length)
+                    headOfAlicesArray = alicesArray[alicesPointer];
+                else
+                    aliceHasGotNoElements = true;
+
+                if (!iHaveGotNoElements && !aliceHasGotNoElements)
+                {
+                    if (headOfMyArray < headOfAlicesArray)
+                    {
+                        mergedArray[i] = headOfMyArray;
+                        myPointer++;
+                    }
+                    else
+                    {
+                        mergedArray[i] = headOfAlicesArray;
+                        alicesPointer++;
+                    }
+                }
+                else if (iHaveGotNoElements)
+                {
+                    mergedArray[i] = headOfAlicesArray;
+                    alicesPointer++;
+                }
+                else if (aliceHasGotNoElements)
+                {
+                    mergedArray[i] = headOfMyArray;
+                    myPointer++;
+                }
+            }
+            return mergedArray;
+        }
+    }
+}
