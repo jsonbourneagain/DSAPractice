31ea401e016cd94617c658fdf90c35f0967c4c85
Json Bourne <jsonbourne@jsonbourneagain@outlook.com>
2020-06-04 13:14:07 +0530

    pushed to master branch

--- DSA/FindADuplicateSpaceEdition/findaduplicate/Program.cs
+++ DSA/FindADuplicateSpaceEdition/findaduplicate/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace findaduplicate
 {
@@ -6,7 +7,30 @@ namespace findaduplicate
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var numbers = new int[] { 1, 2, 3, 2 };
+            var expected = 2;
+            var actual = FindRepeat(numbers);
         }
+
+        public static int FindRepeat(int[] numbers)
+        {
+            // Find a number that appears more than once
+            var numbersSeenSoFar = new HashSet<int>();
+
+            foreach (var number in numbers)
+            {
+                if (numbersSeenSoFar.Contains(number))
+                {
+                    return number;
+                }
+                else
+                {
+                    numbersSeenSoFar.Add(number);
+                }
+            }
+
+            throw new InvalidOperationException("No duplicates found.");
+        }
+
     }
 }
