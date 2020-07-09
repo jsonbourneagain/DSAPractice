dc3d8d1aa744f9c012ce0a63d94250b2503968ea
Lavakesh Pandey <jsonbourneagain@outlook.com>
2020-07-09 18:05:19 +0530

    Added a blank solution for LeetCode practice.

---
+++ DSA/Karatsuba2/Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Numerics;
+
+namespace Karatsuba2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            BigInteger x = BigInteger.Parse("3141592653589793238462643383279502884197169399375105820974944592");
+
+            BigInteger y = BigInteger.Parse("2718281828459045235360287471352662497757247093699959574966967627");
+
+            Console.WriteLine(multiply(x, y));
+            Console.ReadKey();
+        }
+
+        public static BigInteger multiply(BigInteger x, BigInteger y)
+        {
+            BigInteger result = 0;
+
+            int size1 = GetSize(x);
+            int size2 = GetSize(y);
+
+            //find the max size of two integers
+            int N = Math.Max(size1, size2);
+
+            if (N < 2)
+                return x * y;
+
+            //Max length divided by two and rounded up
+            N = (N / 2) + (N % 2);
+
+            //The mulitplying factor for calculating a,b,c,d
+            BigInteger m = (BigInteger)Math.Pow(10, N);
+
+            BigInteger b = x % m;
+            BigInteger a = x / m;
+            BigInteger c = y / m;
+            BigInteger d = y % m;
+
+            BigInteger z0 = multiply(a, c);
+            BigInteger z1 = multiply(b, d);
+            BigInteger z2 = multiply(a + b, c + d);
+
+            result = ((BigInteger)Math.Pow(10, N * 2) * z0) + z1 + ((z2 - z1 - z0) * m);
+            return result;
+        }
+
+        /// <summary>
+        /// returns the size of the BigInteger integers
+        /// </summary>
+        /// <param name="num"></param>
+        /// <returns></returns>
+        public static int GetSize(BigInteger num)
+        {
+            int len = 0;
+            while (num != 0)
+            {
+                len++;
+                num /= 10;
+            }
+            return len;
+        }
+    }
+}
---
+++ DSA/KaratsubaMultiplication/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Numerics;
+
+namespace KaratsubaMultiplication
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            BigInteger x = BigInteger.Parse("3141592653589793238462643383279502884197169399375105820974944592");
+
+            BigInteger y = BigInteger.Parse("2718281828459045235360287471352662497757247093699959574966967627");
+
+            //BigInteger x = BigInteger.Parse("3141592653589793");
+
+            //BigInteger y = BigInteger.Parse("2718281828459045");
+
+            Console.WriteLine(RecIntMul(x, y));
+            Console.ReadKey();
+        }
+
+        // Input: two n-digit positive integers x and y
+        // Output: the product x.y
+        // Assumption: n is a power of 2.
+
+        static BigInteger RecIntMul(BigInteger x, BigInteger y)
+        {
+            // Base case for the recursion
+            if (x < 10 && y < 10)
+                return x * y;
+
+            // Divide the given number in 2 halves, first and second
+            else
+            {
+                // Calculate the number of digits in x & y and take max() of that for computation
+                var n = (int)Math.Max(Math.Floor(BigInteger.Log10(x) + 1), Math.Floor(BigInteger.Log10(y) + 1));
+
+                var m = (n + 1) / 2;
+
+                var a = (x / BigInteger.Parse(BigInteger.Pow(10, m).ToString()));
+                var b = (x % BigInteger.Parse(BigInteger.Pow(10, m).ToString()));
+                var c = (y / BigInteger.Parse(BigInteger.Pow(10, m).ToString()));
+                var d = (y % BigInteger.Parse(BigInteger.Pow(10, m).ToString()));
+
+                BigInteger p = b - a; BigInteger q = c - d;
+
+                var ac = RecIntMul(a, c);
+                var bd = RecIntMul(b, d);
+                var pq = RecIntMul(p, q);
+                var ad = RecIntMul(a, d);
+                var bc = RecIntMul(b, c);
+                //var adbc = pq - ac - bd;
+
+                var result = (BigInteger.Parse(BigInteger.Pow(10, n).ToString()) * ac) + (BigInteger.Parse(BigInteger.Pow(10, m).ToString()) * (ad + bc)) + bd;
+
+                return result;
+            }
+        }
+    }
+}
